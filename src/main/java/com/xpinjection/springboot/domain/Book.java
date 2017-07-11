a13265b8330adc919d8ad0f060a6704fa2bbc1e9
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2017-07-12 01:04:41 +0300

    added Liquibase DB migration support

--- src/main/java/com/xpinjection/springboot/domain/Book.java
+++ src/main/java/com/xpinjection/springboot/domain/Book.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 @RequiredArgsConstructor
 @ToString
 public class Book {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NonNull
