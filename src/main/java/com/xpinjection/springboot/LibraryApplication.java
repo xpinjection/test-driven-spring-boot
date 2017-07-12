f88be583dd2db6b06dea256707d195343b7b77e7
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2017-07-12 11:52:54 +0300

    additional configuration properties and made ready for external container

--- src/main/java/com/xpinjection/springboot/LibraryApplication.java
+++ src/main/java/com/xpinjection/springboot/LibraryApplication.java
@@ -1,14 +1,24 @@
 package com.xpinjection.springboot;
 
 import com.xpinjection.springboot.init.InventorySettings;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(InventorySettings.class)
-public class LibraryApplication {
+public class LibraryApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return configureApplication(builder);
+	}
+
 	public static void main(String[] args) {
-		new SpringApplication(LibraryApplication.class).run(args);
+		configureApplication(new SpringApplicationBuilder()).run(args);
+	}
+
+	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder.sources(LibraryApplication.class);
 	}
 }
