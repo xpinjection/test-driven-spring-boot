32f2842dbf50a9bb7adbbd07dbf8cb28441c0a89
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2018-07-04 00:39:32 +0300

    updated dependencies versions

--- src/main/java/com/xpinjection/springboot/dao/BookDao.java
+++ src/main/java/com/xpinjection/springboot/dao/BookDao.java
@@ -2,6 +2,8 @@ package com.xpinjection.springboot.dao;
 
 import com.xpinjection.springboot.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
