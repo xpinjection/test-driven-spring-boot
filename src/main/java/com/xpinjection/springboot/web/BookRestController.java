c1db3694688e577ffee4fcc289055d7ddace8401
Mikalai Alimenkou <lumii.subscriber@gmail.com>
2020-11-29 18:02:58 +0200

    migrated to Spring Boot 2.3.6 and Java 13

--- src/main/java/com/xpinjection/springboot/web/BookRestController.java
+++ src/main/java/com/xpinjection/springboot/web/BookRestController.java
@@ -4,6 +4,7 @@ import com.xpinjection.springboot.domain.Book;
 import com.xpinjection.springboot.service.BookService;
 import lombok.AllArgsConstructor;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +21,7 @@ import java.util.List;
 public class BookRestController {
     private final BookService bookService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/books")
+    @RequestMapping(method = RequestMethod.GET, path = "/books", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Book> findBooksByAuthor(@RequestParam @NotBlank @Valid String author) {
         return bookService.findBooksByAuthor(author);
     }
--- src/main/java/com/xpinjection/springboot/web/ExpertRestController.java
+++ src/main/java/com/xpinjection/springboot/web/ExpertRestController.java
@@ -5,6 +5,7 @@ import com.xpinjection.springboot.exception.InvalidRecommendationException;
 import com.xpinjection.springboot.service.ExpertService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,7 +15,7 @@ import javax.validation.Valid;
 public class ExpertRestController {
     private final ExpertService service;
 
-    @PostMapping(path = "/experts")
+    @PostMapping(path = "/experts", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     ExpertState addExpert(@RequestBody @Valid Expert expert) {
         long id = service.add(expert);
         return new ExpertState(id);
