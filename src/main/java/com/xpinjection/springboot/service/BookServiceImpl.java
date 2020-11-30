c67e2ef5c3f5e68b0ef60e6f4e7619eeee28974d
Mikalai Alimenkou <lumii.subscriber@gmail.com>
2020-11-30 11:46:30 +0200

    added service level logs and JSON logs profile

--- src/main/java/com/xpinjection/springboot/service/BookServiceImpl.java
+++ src/main/java/com/xpinjection/springboot/service/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.xpinjection.springboot.service;
 
 import com.xpinjection.springboot.adaptors.persistence.BookDao;
 import com.xpinjection.springboot.domain.Book;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import static java.util.stream.Collectors.toList;
 /**
  * @author Alimenkou Mikalai
  */
+@Slf4j
 @Service
 @Transactional
 public class BookServiceImpl implements BookService {
@@ -29,6 +31,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<Book> addBooks(Map<String, String> books) {
+        LOG.info("Adding books: {}", books);
         return books.entrySet().stream()
                 .map(entry -> new Book(entry.getKey(), entry.getValue()))
                 .map(bookDao::save)
@@ -37,6 +40,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<Book> findBooksByAuthor(String author) {
+        LOG.info("Try to find books by author: {}", author);
         Assert.hasText(author, "Author is empty!");
         var normalizedAuthor = normalizeAuthorName(author);
         return cache.computeIfAbsent(normalizedAuthor, bookDao::findByAuthor);
@@ -63,6 +67,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<Book> findAllBooks() {
+        LOG.info("Finding all books");
         return bookDao.findAll();
     }
 }
--- src/main/java/com/xpinjection/springboot/service/ExpertServiceImpl.java
+++ src/main/java/com/xpinjection/springboot/service/ExpertServiceImpl.java
@@ -8,6 +8,7 @@ import com.xpinjection.springboot.domain.Expert;
 import com.xpinjection.springboot.domain.Recommendation;
 import com.xpinjection.springboot.exception.InvalidRecommendationException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,20 +18,24 @@ import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 
-@AllArgsConstructor
+@Slf4j
 @Service
 @Transactional
+@AllArgsConstructor
 public class ExpertServiceImpl implements ExpertService {
     private final BookDao bookDao;
     private final ExpertDao expertDao;
 
     @Override
     public long add(Expert expert) {
+        LOG.info("Add expert: {}", expert);
         var books = findRecommendedBooks(expert);
         if (books.isEmpty()) {
+            LOG.error("Recommended books are not found");
             throw new InvalidRecommendationException("No valid recommendations");
         }
         var saved = storeExpert(expert, books);
+        LOG.info("Expert is stored with ID: {}", saved.getId());
         return saved.getId();
     }
 
@@ -56,10 +61,11 @@ public class ExpertServiceImpl implements ExpertService {
                         validateAuthor(parts[1], book));
     }
 
-    private Optional<Book> findBookByName(String part) {
+    private Optional<Book> findBookByName(String name) {
         try {
-            return bookDao.findByName(part);
+            return bookDao.findByName(name);
         } catch (IncorrectResultSizeDataAccessException e) {
+            LOG.error("Recommended book exists in several versions: {}", e.getActualSize());
             throw new InvalidRecommendationException("Recommended book exists in several versions: "
                     + e.getActualSize());
         }
@@ -67,6 +73,7 @@ public class ExpertServiceImpl implements ExpertService {
 
     private boolean validateAuthor(String author, Book book) {
         if (!book.getAuthor().equals(author)) {
+            LOG.error("Author {} doesn't map for the book: {}", author, book);
             throw new InvalidRecommendationException("Invalid author: " + author);
         }
         return true;
