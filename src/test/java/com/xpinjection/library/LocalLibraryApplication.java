291cf3c87a7c8a47e457408c8bb51e8eb43518e9
Mikalai Alimenkou <lumii.subscriber@gmail.com>
2021-05-20 18:48:37 +0300

    updated to Spring Boot 2.4.5, configured startup analyzer

--- src/test/java/com/xpinjection/library/LocalLibraryApplication.java
+++ src/test/java/com/xpinjection/library/LocalLibraryApplication.java
@@ -1,11 +1,14 @@
 package com.xpinjection.library;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 public class LocalLibraryApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(LibraryApplication.class)
                 .initializers(new StandaloneApplicationContextInitializer())
+                .applicationStartup(new BufferingApplicationStartup(2048))
+                //.applicationStartup(new FlightRecorderApplicationStartup())
                 .profiles("dev")
                 .run(args);
     }
