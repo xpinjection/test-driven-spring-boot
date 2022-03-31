98c8c46f11971ea5f5aea0741cdb0f9d425bd3eb
Mikalai Alimenkou <lumii.subscriber@gmail.com>
2022-03-31 21:09:31 +0200

    removed basic auth from health probes

--- src/main/java/com/xpinjection/library/config/ActuatorBasicSecurityConfig.java
+++ src/main/java/com/xpinjection/library/config/ActuatorBasicSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.xpinjection.library.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 @RequiredArgsConstructor
 public class ActuatorBasicSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final WebEndpointProperties webEndpointProperties;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -16,7 +19,8 @@ public class ActuatorBasicSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
             .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers(webEndpointProperties.getBasePath().concat("/health/**")).permitAll()
+                .antMatchers(webEndpointProperties.getBasePath().concat("/**")).hasRole("ADMIN")
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
