12553c13147d7a1d589a17faa919fa713347f5f4
Mikalai Alimenkou <lumii.subscriber@gmail.com>
2022-06-28 15:54:46 +0200

    added API validator, configured ArchUnit tests in Maven

--- src/test/java/com/xpinjection/library/adaptors/api/AbstractApiTest.java
+++ src/test/java/com/xpinjection/library/adaptors/api/AbstractApiTest.java
@@ -2,6 +2,7 @@ package com.xpinjection.library.adaptors.api;
 
 import com.github.database.rider.spring.api.DBRider;
 import io.restassured.RestAssured;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -10,13 +11,17 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@ContextConfiguration(initializers = StandaloneApplicationContextInitializer.class)
 @DBRider
+@Slf4j
 @ActiveProfiles("test")
 public abstract class AbstractApiTest {
+    private static OpenApiValidator OPEN_API_VALIDATOR = new OpenApiValidator();
+
     @LocalServerPort
     protected int port;
 
     @BeforeEach
     void init() {
         RestAssured.port = port;
+        OPEN_API_VALIDATOR.validate(port);
     }
 }
