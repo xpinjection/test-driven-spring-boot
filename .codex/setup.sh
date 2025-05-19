e13c1d14731a1c9448b72da7cf21be181a7bcfbf
Mikalai Alimenkou <lumii.subscriber@gmail.com>
2025-05-19 20:01:56 +0200

    Added echo to setup script

--- .codex/setup.sh
+++ .codex/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Running setup"
 cd ..
 chmod +x mvnw
+echo "Download Maven dependencies for local execution"
 ./mvnw clean package -Dtestcontainers.enabled=false
+echo "Setup finished successfully"
