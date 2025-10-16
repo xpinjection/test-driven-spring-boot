4f077dc5b53a407cec1dc909cae249c5e50ce233
Mikalai Alimenkou <mikalai.alimenkou@gmail.com>
2025-10-16 17:49:00 +0200

    added dependencies info, fixed compose container names

--- src/main/java/com/xpinjection/library/LibraryApplication.java
+++ src/main/java/com/xpinjection/library/LibraryApplication.java
@@ -1,14 +1,17 @@
 package com.xpinjection.library;
 
 import com.xpinjection.library.config.LibrarySettings;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(LibrarySettings.class)
 public class LibraryApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
+        new SpringApplicationBuilder(LibraryApplication.class)
+                .applicationStartup(new BufferingApplicationStartup(2048))
+                .run(args);
+    }
 }
